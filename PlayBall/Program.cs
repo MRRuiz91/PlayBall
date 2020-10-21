fbdaef750006a7b380c2dfb89bbafa520985a363
MRRuiz91 <70775050+MRRuiz91@users.noreply.github.com>
2020-10-21 13:06:32 -0400

    Updated version

--- PlayBall/Program.cs
+++ PlayBall/Program.cs
@@ -11,13 +11,6 @@ namespace PlayBall
         private const string MAIN_MENU_OPTION_PLAY = "Play";
         private const string MAIN_MENU_OPTION_EXIT = "Exit";
         private readonly string[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_PLAY, MAIN_MENU_OPTION_EXIT };
-
-        //private const string SUB_MENU_PLAY_SLOW_PITCH = "0 - 1, Slow Pitch";
-        //private const string SUB_MENU_PLAY_STRAIGHT_PITCH = "2 - 3, Straight Pitch";
-        //private const string SUB_MENU_PLAY_FASTBALL = "4 - 6, Fastball";
-        //private const string SUB_MENU_PLAY_CURVEBALL = "7 - 9, Curveball";
-        //private readonly string[] SUB_MENU_OPTIONS = { SUB_MENU_PLAY_SLOW_PITCH, SUB_MENU_PLAY_STRAIGHT_PITCH, SUB_MENU_PLAY_FASTBALL, SUB_MENU_PLAY_CURVEBALL };
-
         private readonly IBasicUserInterface ui = new MenuDrivenCLI();
 
         static void Main(string[] args)
@@ -28,7 +21,6 @@ namespace PlayBall
 
         public void Run()
         {        
-            PlayBall playball = new PlayBall(); //Not using yet
             bool exit = false;
             Console.WriteLine("Hello! Let's play some baseball!");
             Console.WriteLine("What would you like to do?");
@@ -48,7 +40,7 @@ namespace PlayBall
                     Console.WriteLine("Please enter your jersey number:");
                     string jerseyNumber = Console.ReadLine();
                     Console.WriteLine("Introducing number " + jerseyNumber + ", " + userName.ToUpper() + "!");
-                    while (strikeCount < 4 || hitCount <4)
+                    while (strikeCount < 4 || hitCount < 4)
                     {
                         PromptForPitch();
                         try
@@ -56,9 +48,7 @@ namespace PlayBall
                             int pitchStyle = int.Parse(Console.ReadLine());
                             Random rand = new Random();
 
-                            //selection = (string)ui.PromptForSelection(SUB_MENU_OPTIONS);
-
-                            if (pitchStyle == 0 || pitchStyle == 1) //could move pitchstyle to another class(playball) to figure it out
+                            if (pitchStyle == 0 || pitchStyle == 1)
                             {
                                 int genNum = rand.Next(0, 2);
                                 HitOrStrike(ref hitCount, ref strikeCount, pitchStyle, genNum);
@@ -118,7 +108,7 @@ namespace PlayBall
 
         private static void PromptForPitch()
         {
-            Console.WriteLine("Please enter a number corresponding to your pitch choice:"); //extract these into a method to promptforpitch
+            Console.WriteLine("Please enter a number corresponding to your pitch choice:"); 
             Console.WriteLine("0 - 1, Slow Pitch");
             Console.WriteLine("2 - 3, Straight Pitch");
             Console.WriteLine("4 - 6, Fastball");
